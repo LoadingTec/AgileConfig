00b27682da78a7beba0d3c78d2b4ee305de2842a
kklldog <kklldog@gmail.com>
2026-02-19 19:14:14 +0000

    feat: Add built-in plugin registration helpers
    - Add RegisterBuiltInPlugins extension method
    - Include Etcd and Consul plugin registration samples
    - Plugins disabled by default, can be enabled via config

--- src/AgileConfig.Server.SyncPlugin/SyncPluginExtensions.cs
+++ src/AgileConfig.Server.SyncPlugin/SyncPluginExtensions.cs
@@ -1,10 +1,13 @@
-using Microsoft.Extensions.DependencyInjection;
 using AgileConfig.Server.SyncPlugin;
+using AgileConfig.Server.SyncPlugin.Plugins.Consul;
+using AgileConfig.Server.SyncPlugin.Plugins.Etcd;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace AgileConfig.Server.SyncPlugin;
 
 /// <summary>
-/// Extension methods for registering SyncPlugin services
+/// Extension methods for registering SyncPlugin services and plugins
 /// </summary>
 public static class SyncPluginExtensions
 {
@@ -16,5 +19,40 @@ public static class SyncPluginExtensions
         services.AddSingleton<SyncEngine>();
 
         return services;
+    }
+
+    /// <summary>
+    /// Register built-in sync plugins (call after AddSyncPlugin)
+    /// This is typically called during app startup
+    /// </summary>
+    public static void RegisterBuiltInPlugins(SyncEngine syncEngine, ILoggerFactory loggerFactory)
+    {
+        // Register Etcd plugin
+        var etcdLogger = loggerFactory.CreateLogger<EtcdSyncPlugin>();
+        var etcdPlugin = new EtcdSyncPlugin(etcdLogger);
+        syncEngine.RegisterPlugin(etcdPlugin, new SyncPluginConfig
+        {
+            PluginName = "etcd",
+            Enabled = "false", // Disabled by default
+            Settings = new Dictionary<string, string>
+            {
+                { "endpoints", "http://localhost:2379" },
+                { "keyPrefix", "/agileconfig" }
+            }
+        });
+
+        // Register Consul plugin
+        var consulLogger = loggerFactory.CreateLogger<ConsulSyncPlugin>();
+        var consulPlugin = new ConsulSyncPlugin(consulLogger);
+        syncEngine.RegisterPlugin(consulPlugin, new SyncPluginConfig
+        {
+            PluginName = "consul",
+            Enabled = "false", // Disabled by default
+            Settings = new Dictionary<string, string>
+            {
+                { "address", "http://localhost:8500" },
+                { "keyPrefix", "agileconfig" }
+            }
+        });
     }
 }
